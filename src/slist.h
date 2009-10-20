0056faf69987a7a25048a50a65ceac00c02085ed
Maxwell Carey <maxwellhaydn@gmail.com>
2009-10-19 21:49:10 -0600

    Added doxygen/javadoc style comments.

--- src/slist.h
+++ src/slist.h
@@ -4,31 +4,62 @@
 
 class SList {
   public:
-    /* Default constructor */
+
+    /**
+     * Default constructor. Constructs an empty list.
+     */
     SList() : head(0) { }
 
-    /* Copy constructor */
+    /**
+     * Copy constructor.
+     *
+     * @param other the list to be copied
+     */
     SList(const SList& other);
 
-    /* Destructor */
+    /**
+     * Destructor.
+     */
     ~SList();
 
-    /* Assignment operator */
+    /**
+     * Assignment operator.
+     *
+     * @param other the list to assign from
+     */
     //SList& operator=(const SList& other);
 
-    /* Append an item to the list */
+    /**
+     * Append an item to the end of the list.
+     *
+     * @param value the value of the item to append
+     */
     void append(int value);
 
-    /* Clear all items from the list */
+    /**
+     * Remove all items from the list.
+     */
     void clear();
 
+    /**
+     * Check whether the list is empty.
+     *
+     * @return whether the list is empty
+     */
     bool isEmpty() const { return head == 0; }
 
-    /* Print the contents of the list in order */
+    /**
+     * Print the contents of the list to stdout in order from first to
+     * last.
+     */
     void printContents();
 
   private:
-    /* Get the head of the list */
+    /**
+     * Get the head of the list (i.e. get the first node).
+     * 
+     * @return the head node
+     */
     Node *getHead() const { return head; } // Maybe this should be protected?
 
     Node *head;
