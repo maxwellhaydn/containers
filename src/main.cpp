128a161f0a05ac436eccf474e2ed726580a9b00d
Maxwell Carey <maxwellhaydn@gmail.com>
2009-10-20 10:27:26 -0600

    Templated SList class.
    This involved removing slist.cpp and placing all implementation in slist.h.

--- src/main.cpp
+++ src/main.cpp
@@ -2,11 +2,11 @@
 #include "slist.h"
 
 int main(int argc, char* argv[]) {
-  SList list1;
+  SList<int> list1;
   list1.append(1);
   list1.append(2);
   list1.append(7); 
-  SList list2(list1);
+  SList<int> list2(list1);
 
 
   std::cout << "list1:" << std::endl;
--- src/slist.h
+++ src/slist.h
@@ -2,6 +2,7 @@
 
 #include "node.h"
 
+template <typename T>
 class SList {
   public:
 
@@ -15,7 +16,7 @@ class SList {
      *
      * @param other the list to be copied
      */
-    SList(const SList& other);
+    SList(const SList<T>& other);
 
     /**
      * Destructor.
@@ -27,14 +28,14 @@ class SList {
      *
      * @param other the list to assign from
      */
-    SList& operator=(const SList& other);
+    SList<T>& operator=(const SList<T>& other);
 
     /**
      * Append an item to the end of the list.
      *
      * @param value the value of the item to append
      */
-    void append(int value);
+    void append(T value);
 
     /**
      * Remove all items from the list.
@@ -60,17 +61,100 @@ class SList {
      * 
      * @return the head node
      */
-    Node* getHead() const;  // Maybe this should be protected?
+    Node<T>* getHead() const;  // Maybe this should be protected?
 
-    Node *head;
+    Node<T> *head;
 };
 
-/* Inline function definitions */
-inline bool SList::isEmpty() const {
+template <typename T>
+SList<T>::SList(const SList<T>& other) {
+  head = 0;
+
+  if (other.isEmpty()) {
+    return;
+  }
+
+  Node<T> *current = 0;  // Pointer to the node being copied from other list
+  Node<T> *last = 0; // Pointer to the last node added to this list
+
+  // First copy head node of other list
+  current = other.getHead();
+  last = new Node<T>(current->getValue());
+  head = last;
+
+  // Traverse other list, copying each node along the way
+  while (current->hasNext()) {
+    current = current->getNext();
+    last->setNext(new Node<T>(current->getValue()));
+    last = last->getNext();
+  }
+}
+
+template <typename T>
+SList<T>::~SList() {
+  // Calling the destructor for the head node will in turn call the
+  // destructor for each node in the list
+  delete head;
+}
+
+/*template <typename T>
+SList& SList<T>::operator=(const SList& other) {
+  // Check for self-assignment
+  if (this != &other) {
+        
+  }
+
+  return *this;
+}*/
+
+template <typename T>
+void SList<T>::append(T value) {
+  Node<T> *newNode = new Node<T>(value);
+
+  if (isEmpty()) {
+    head = newNode;
+    return;
+  }
+
+  // Traverse the list to the last node
+  Node<T> *current = head;
+  while (current->hasNext()) {
+    current = current->getNext();
+  }
+
+  // Append new node to the last node
+  current->setNext(newNode);
+}
+
+template <typename T>
+void SList<T>::clear() {
+  // Calling the destructor for the head node will in turn call the
+  // destructor for each node in the list
+  delete head;
+  head = 0;
+}
+
+template <typename T>
+inline bool SList<T>::isEmpty() const {
   return head == 0;
 }
 
-inline Node* SList::getHead() const {
+template <typename T>
+void SList<T>::printContents() {
+  if (isEmpty()) {
+    std::cout << "Empty List" << std::endl;
+    return;
+  }
+
+  Node<T> *current = head;
+  do {
+    std::cout << current->getValue() << std::endl;
+    current = current->getNext();
+  } while (current != 0);
+}
+
+template <typename T>
+inline Node<T>* SList<T>::getHead() const {
   return head;
 }
 
