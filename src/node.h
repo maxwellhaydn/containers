3eb992dff9b26e59d5d2f35a71881dfbca001aab
Maxwell Carey <maxwellhaydn@gmail.com>
2009-10-19 18:42:25 -0600

    Added Javadoc-style comments to node.h.

--- src/node.h
+++ src/node.h
@@ -2,20 +2,52 @@
 
 class Node {
   public:
-    /* Default constructor */
+    /**
+     * Default constructor.
+     *
+     * @param v the value to be stored in this node
+     */
     Node(int v = 0) : next(0), value(v) { }
 
-    /* Destructor */
+    /**
+     * Destructor.
+     */
     ~Node();
 
-    /* Modifiers */
+    /**
+     * Set the next pointer of this node.
+     *
+     * @param n the next node
+     */
     void setNext(Node *n) { next = n; }
+
+    /**
+     * Set the value of this node.
+     *
+     * @param v the value
+     */
     void setValue(int v) { value = v; }
 
-    /* Accessors */
+    /**
+     * Get the next pointer of this node.
+     *
+     * @return the next pointer
+     */
     Node* getNext() const { return next; }
+
+    /**
+     * Get the value of this node.
+     *
+     * @return the value
+     */
     int getValue() const { return value; }
 
+    /**
+     * Whether this node's next pointer points to another node (i.e. has a
+     * non-null next pointer).
+     *
+     * @return whether the next pointer is non-null
+     */
     bool hasNext() const { return next != 0; }
 
   private:
